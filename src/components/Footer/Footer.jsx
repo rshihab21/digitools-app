8dcc5905254b705aa2bbc5311de86b63a1fb1b70
rshihab21 <shihabcse21@gmail.com>
2026-04-01 15:13:46 +0600

    fix footer design according to figma

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { CiInstagram } from 'react-icons/ci'
+import { FaFacebookSquare } from 'react-icons/fa'
+import { FaSquareXTwitter, FaXTwitter } from 'react-icons/fa6'
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-900 text-gray-300 px-6 py-12">
+        <footer className="w-full bg-gray-900 text-gray-300 px-6 py-12">
             <div className="max-w-6xl mx-auto grid md:grid-cols-5 gap-8">
                 <div>
-                    <h2 className="text-white text-xl font-bold mb-4">Digitools</h2>
+                    <h2 className="text-white text-2xl font-bold mb-4">DigiTools</h2>
                     <p className="text-sm">
                         Powerful tools to help you work smarter and grow faster. Built for professionals and teams.
                     </p>
@@ -39,19 +42,27 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div>
-                    <h3 className="text-white font-semibold mb-4">Social</h3>
-                    <ul className="space-y-2 text-sm">
-                        <li className="hover:text-white cursor-pointer">Help Center</li>
-                        <li className="hover:text-white cursor-pointer">Terms of Service</li>
-                        <li className="hover:text-white cursor-pointer">Privacy Policy</li>
-                    </ul>
+                    <h3 className="text-white font-semibold mb-4">Social Links</h3>
+                  <div className='flex gap-4'>
+                    <span className='bg-white p-2 rounded-full text-black'><CiInstagram /></span>
+                    <span className='bg-white p-2 rounded-full text-black'><FaFacebookSquare /></span>
+                    <span className='bg-white p-2 rounded-full text-black'><FaXTwitter /></span>
+                  </div>
+                   
                 </div>
 
             </div>
 
             {/* Bottom */}
-            <div className="border-t border-gray-700 mt-10 pt-6 text-center text-sm text-gray-400">
-                © {new Date().getFullYear()} Digitools App. All rights reserved.
+            <div className="border-t border-gray-700 mt-10 pt-6 text-center text-sm text-gray-400 container mx-auto">
+               <div className='md:flex justify-between gap-4'>
+                <p> © {new Date().getFullYear()} Digitools App. All rights reserved.</p>
+                <div className='md:flex gap-4'>
+                    <span className='mr-2'>Privacy Policy</span>
+                    <span className='mr-2'>Terms of Service</span>
+                    <span>Cookies</span>
+                </div>
+               </div>
             </div>
         </footer>
     )
